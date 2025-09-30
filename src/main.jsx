300964464049b9da7a756ef9a285896cb958807f
nolann973 <a.nolann973@gmail.com>
2025-09-30 11:54:56 -0300

    Initial commit BudgetApp

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import BudgetApp from './BudgetApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BudgetApp />
+  </React.StrictMode>
+);
